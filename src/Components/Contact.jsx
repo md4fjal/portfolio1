140bf1e4ebf77d659577961f3f6e1d012c9b20a3
Mohd Afjal <afjal742000@gmail.com>
2023-09-28 22:25:26 +0530

    firebase

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,10 +2,43 @@ import React, { useState } from "react";
 import vg from "../Assets/logo.png";
 import { motion } from "framer-motion";
 const Contact = () => {
-  const [name] = useState("");
-  const [email] = useState("");
-  const [message] = useState("");
-  
+  const [user,setUser] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const getData = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({...user, [name] : value});
+    
+  }
+
+  const {name, email, message} = user;
+  const postData = async (e) => {
+    e.preventDefault();
+    const res = await fetch('https://react-portfolio-b9c12-default-rtdb.firebaseio.com/userDatail.json', {
+      method: "POST",
+      Headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        message
+      })
+    })
+    if(res) {
+      setUser({
+        name: "",
+        email: "",
+        message: ""
+      })
+      alert("Data Sent Successfully")
+    }
+  }
 
   const animations = {
     form: {
@@ -36,33 +69,40 @@ const Contact = () => {
   return (
     <div id="contact">
       <section>
-        <motion.form {...animations.form}>
+        <motion.form {...animations.form} method="POST">
           <h2>Contact Me</h2>
           <input
             type="text"
-            value={name}
-            
+            name="name"
+            value={user.name}
+            onChange={getData}
             placeholder="Your Name"
             required
+            autoComplete="off"
           />
           <input
             type="email"
+            name="email"
             placeholder="Your Email"
+            value={user.email}
+            onChange={getData}
             required
-            value={email}
-            
+            autoComplete="off"
           />
           <input
             type="text"
+            name="message"
             placeholder="Your Message"
             required
-            value={message}
-            
+            value={user.message}
+            onChange={getData}
+            autoComplete="off"
           />
 
           <motion.button
             {...animations.button}
             type="submit"
+            onClick={postData}
           >
             Send
           </motion.button>
